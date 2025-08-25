fc5006c0a7a4cce7940dc8ecdf0daf39c4a1d353
uvaish7651 <uvaishraza000@gmail.com>
2025-08-25 00:06:34 -0500

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-    origin: ["http://localhost:5173"],
+    origin: ["https://job-jodi-v4eh.onrender.com"],
     credentials: true,
 };
 
